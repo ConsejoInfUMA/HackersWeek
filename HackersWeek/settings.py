a9b59a904f204907552adeabe6e8c16d0a13fe92
Francisco Baena <soy@franbaena.com>
2015-02-04 16:32:16 +0000

    Added application www, home & base templates/CSS and changed some settings.

--- HackersWeek/settings.py
+++ HackersWeek/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Installed www application
+    'www'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -66,9 +68,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
