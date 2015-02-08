63f938303cc3c7fed1a58e7fc58633ba3e35ad47
Francisco Baena <soy@franbaena.com>
2015-02-08 19:52:20 +0000

    Added model «User Profile» + template create_profile.html

--- www/models.py
+++ www/models.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+COURSE_CHOICES = (
+    ('GII', 'Grado en Ingeniería Informática'),
+    ('GIS', 'Grado en Ingeniería del Software'),
+    ('GIC', 'Grado en Ingeniería de Computadores'),
+    ('GISa', 'Grado en Ingeniería de la Salud'),
+    ('II', 'Ingeniería Informática (plan antiguo)'),
+    ('ITS', 'Ingeniería Técnica Informática de Sistemas'),
+    ('ITG', 'Ingeniería Técnica Informática de Gestión'),
+    ('OTH', 'Otra Titulación'),
+    ('NOS', 'No soy estudiante de la UMA'),
+)
+
+class UserProfile(models.Model):
+	"""
+	Extended profile of an User
+	"""
+	user = models.OneToOneField(User)
+	dni = models.CharField(max_length=25)
+	course = models.CharField(max_length=3, choices=COURSE_CHOICES, default='II')
+	auto_enroll_all = models.BooleanField(default=False)
+
+	def __src__(self):
+		return "%s" % self.user
--- www/views.py
+++ www/views.py
@@ -1,17 +1,26 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.contrib import messages
 from django.contrib.messages import get_messages
+from .models import *
 
 
 
 def home(request):
+	if request.user.is_authenticated:
+		pass
+		#need to try
+
 	context = {}
 	return render(request, 'home.html', context)
 
 def activity(request):
+	if request.user.is_authenticated:
+		pass
+		#need to try
 	context = {}
 	return render(request, 'activity.html', context)
 
 def create_profile(request):
-	context = {}
+	context = {'course_choices':COURSE_CHOICES}
 	return render(request, 'create_profile.html', context)
