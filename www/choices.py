d76ea4214a21784ad2f1e7c05885603bfcc5b7f1
Francisco Baena <soy@franbaena.com>
2015-02-09 03:10:20 +0000

    Added models for Venue, TimeStripe and Event

---
+++ www/choices.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+'''
+Choices
+'''
+
+COURSE_CHOICES = (
+    ('GII', 'Grado en Ingeniería Informática'),
+    ('GIS', 'Grado en Ingeniería del Software'),
+    ('GIC', 'Grado en Ingeniería de Computadores'),
+    ('GISa', 'Grado en Ingeniería de la Salud'),
+    ('II', 'Ingeniería Informática (plan antiguo)'),
+    ('ITS', 'Ingeniería Técnica Informática de Sistemas'),
+    ('ITG', 'Ingeniería Técnica Informática de Gestión'),
+    ('OTH', 'Otra Titulación'),
+    ('NOS', 'No soy estudiante de la UMA'),
+)
+
+KIND_OF_EVENT_CHOICES = (
+    ('C', 'Conferencia'),
+    ('W', 'Taller'),
+    ('GIC', 'Grado en Ingeniería de Computadores'),
+    ('GISa', 'Grado en Ingeniería de la Salud'),
+    ('II', 'Ingeniería Informática (plan antiguo)'),
+    ('ITS', 'Ingeniería Técnica Informática de Sistemas'),
+    ('ITG', 'Ingeniería Técnica Informática de Gestión'),
+    ('OTH', 'Otra Titulación'),
+    ('NOS', 'No soy estudiante de la UMA'),
+)
+
+TIME_STRIPE_CHOICES = (
+    (1, '10:30'),
+    (2, '11:30'),
+    (3, '12:30'),
+    (4, '15:30'),
+    (5, '16:30'),
+    (6, '17:30'),
+    (7, '18:30'),
+)
+
+DURATION_CHOICES = (
+    (1, '1 hora'),
+    (2, '2 horas'),
+)
+
+DAY_CHOICES = (
+    (1, 'Lunes'),
+    (2, 'Martes'),
+    (3, 'Miércoles'),
+    (4, 'Jueves'),
+)
--- www/models.py
+++ www/models.py
@@ -1,18 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
-
-COURSE_CHOICES = (
-    ('GII', 'Grado en Ingeniería Informática'),
-    ('GIS', 'Grado en Ingeniería del Software'),
-    ('GIC', 'Grado en Ingeniería de Computadores'),
-    ('GISa', 'Grado en Ingeniería de la Salud'),
-    ('II', 'Ingeniería Informática (plan antiguo)'),
-    ('ITS', 'Ingeniería Técnica Informática de Sistemas'),
-    ('ITG', 'Ingeniería Técnica Informática de Gestión'),
-    ('OTH', 'Otra Titulación'),
-    ('NOS', 'No soy estudiante de la UMA'),
-)
+from .choices import *
 
 class UserProfile(models.Model):
 	"""
@@ -23,5 +12,48 @@ class UserProfile(models.Model):
 	course = models.CharField(max_length=3, choices=COURSE_CHOICES, default='II')
 	auto_enroll_all = models.BooleanField(default=False)
 
-	def __src__(self):
+	def __unicode__(self):
 		return "%s" % self.user
+
+class Venue(models.Model):
+	"""
+	Model for a Venue
+	"""
+	name = models.CharField(max_length=255)
+	latitude = models.CharField(max_length=255)
+	longitude = models.CharField(max_length=255)
+	
+	def __unicode__(self):
+		return "%s" % self.name
+
+class TimeStripe(models.Model):
+	"""
+	Model for a Calendar Stripe
+	"""
+	day = models.IntegerField(max_length=1, choices=DAY_CHOICES, default=1)
+	time_stripe = models.IntegerField(max_length=1, choices=TIME_STRIPE_CHOICES, default=1)
+	duration = models.IntegerField(max_length=1, choices=DURATION_CHOICES, default=2)
+	
+	def __unicode__(self):
+		return "(%s el %s @ %s)" % (self.get_duration_display(), self.get_day_display(), self.get_time_stripe_display())
+	
+	class Meta:
+		ordering = ['day','time_stripe']
+
+class Event(models.Model):
+	"""
+	Model for an event
+	"""
+	kind_of_event = models.CharField(max_length=1, choices=KIND_OF_EVENT_CHOICES, default='C')
+	name = models.CharField(max_length=255)
+	speaker = models.CharField(max_length=255, blank=True, null=True)
+	company = models.CharField(max_length=255, blank=True, null=True)
+	venue = models.ForeignKey(Venue)
+	time = models.ManyToManyField(TimeStripe)
+	description = models.TextField()
+	instructions = models.TextField(blank=True, null=True)
+	capacity = models.IntegerField(max_length=4, blank=True, null=True)
+	slug = models.SlugField()
+
+	def __unicode__(self):
+		return "<%s: %s>" % (self.get_kind_of_event_display(), self.name)
